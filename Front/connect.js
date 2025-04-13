0a2309c2443a8ae6e57b701b1027ec32821bee6b
mives2 <mives458@gmail.com>
2025-04-13 03:34:44 -0500

    Add Initial Insert Button with Mock Prompt

--- Front/connect.js
+++ Front/connect.js
@@ -1,4 +1,20 @@
+console.log("connect.js is loaded!");
+let root = null;
+
+function init() {
+    const container = document.getElementById("java");
+    root = ReactDOM.createRoot(container);
+
+    loadResponse();
+
+    const insertBtn = document.getElementById("insert-btn");
+    insertBtn.addEventListener("click", () => {
+        rend("insert");
+    });
+}
+
 function sendReq() {
+    console.log("Sending fetch request...");
     return fetch ("http://localhost:8080/hello", {
         method: "GET",
         headers: {
@@ -14,8 +30,20 @@ function sendReq() {
 }
 
 async function loadResponse() {
-    const container = document.getElementById("java");
-    const root = ReactDOM.createRoot(container);
+    console.log("loadResponse called")
     const res = await sendReq();
     root.render(res);
 }
+
+function insert() {
+    const insert = <form><label for='insert'>Please provide which table you would like to insert into: </label><input id='insert'></input></form>;
+    return insert;
+}
+
+function rend(fun) {
+    if (fun == "insert") {
+        root.render(insert());
+    }
+}
+
+init();
